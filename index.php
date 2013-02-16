c0e0ea6c3041edc8693b888fce681445966d9db6
OpenShift guest <hcouch@bulldogs.mgccc.edu>
2013-02-15 23:42:47 -0500

    Heroku php edit

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <meta charset="UTF-8" />
+  <title>Pizza Planet</title>
+  <link rel="stylesheet" href="css/style.css" />
+  <link rel="stylesheet" href="css/colors.css" />
+ </head>
+ 
+ <body>
+
+  <div id="wrapper">
+
+      <div id="header">
+      <a href="index.html"><img src="images/banner.png"/></a>
+      </div>
+
+   
+
+      <div id="container">
+          <div id="nav">
+          <ul>
+          <li><a href="index.html">Home</a></li>
+          <li><a href="about.html">About</a></li>
+          <li><a href="menu.html">Menu</a></li>
+          <li><a href="contact.html">Contact</a></li>
+          <li><a href="career.html">Join Our Team</a></li>
+          </ul>
+          </div>
+          <div id="main">
+    
+               <div id="window">
+                <div id="info">
+               <h1>Welcome to the Pizza Planet website!</h1>
+               <h2>Thanks for visiting us! </h2>
+    
+               <p>We hope you enjoy your visit. You can learn more about us by visiting our About page or you can
+                  view our menu. You also can send us a note by visiting the contact page.</p>
+               <p>We are currently looking for eager new faces to join our family. If you enjoy working in a fast-paced and friendly
+               enviroment, click the Join Us link to get started.</p>
+               </div>
+          </div>
+    
+    
+         </div>   
+     </div>
+  
+   
+   
+
+
+     <div id=footer>
+     <p>&copy; 2013 Pizza Planet | Harrison Couch </p>
+     </div>
+
+  </div>
+</body>
+</html>
---
+++ thanks.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <meta charset="UTF-8" />
+  <title>Pizza Planet</title>
+    <link rel="stylesheet" href="css/style.css" />
+  <link rel="stylesheet" href="css/colors.css" />
+
+ </head>
+ <body>
+
+  <div id="wrapper">
+
+            <div id="header">
+      <a href="index.html"><img src="images/banner.png"/></a>
+      </div>
+   
+
+      <div id="container">
+          <div id="nav">
+          <ul>
+          <li><a href="index.html">Home</a></li>
+          <li><a href="about.html">About</a></li>
+          <li><a href="menu.html">Menu</a></li>
+          <li><a href="contact.html">Contact</a></li>
+          <li><a href="career.html">Join Our Team</a></li>
+          </ul>
+          </div>
+          <div id="main">
+           <div id="window">
+           <h1>Thank You!</h1>
+           <p>We appreciate you talking the time to write us. We will work hard to get a response to you as quickly as possible. </div>
+   </div>
+   
+   </div>
+  
+   
+   
+
+
+   <div id=footer>
+    <p>&copy; 2013 Pizza Planet | Harrison Couch </p>
+   </div>
+
+   </div>
+ </body>
+</html>
